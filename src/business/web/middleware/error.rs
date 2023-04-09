539e53eba0cf190c016a83413aef5bca98f443d1
Adam Vincent <adam.vincent@totalprocessing.com>
2023-04-09 15:58:44 +0100

    add error middleware

--- src/business/web/middleware/error.rs
+++ src/business/web/middleware/error.rs
@@ -18,15 +18,58 @@ pub async fn error<B>(
 
     let response = next.run(request).await;
 
-    // TODO - need to find a way to pull in the request error type that has been propergated here in order
-    // TODO - to handle it and return a response.
+    let status = response.status().as_u16();
 
-    if response.status().as_u16() != 200 {
-        context.log.warn_w(
-            "status code not 200 returned, so we should properly handle API errors",
-            Some(()),
-        );
+    match status {
+        200..=299 => Ok(response),
+        _ => {
+            // We know its an error now so we can properly decode the body to return the messages we want too.
+
+            let data = match hyper::body::to_bytes(response.into_body()).await {
+                Ok(data) => data,
+                Err(err) => {
+                    context
+                        .log
+                        .error_w("could not convert to bytes in error middleware", Some(err));
+                    return Err(RequestError::new(
+                        axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                        "Internal Server Error",
+                    ));
+                }
+            };
+
+            let data = match std::str::from_utf8(&data) {
+                Ok(data) => data,
+                Err(err) => {
+                    context
+                        .log
+                        .error_w("could no read bytes in error middleware ", Some(err));
+                    return Err(RequestError::new(
+                        axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                        "Internal Server Error",
+                    ));
+                }
+            };
+
+            // We can now log the error message to the console, so we know the reason for the 500 error, but the user does not.
+            context.log.warn_w(&data.to_string(), Some(()));
+
+            // We only return the bytes if the status code is 400-499.
+
+            match status {
+                400..=499 => {
+                    return Err(RequestError::new(
+                        axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                        data.to_string(),
+                    ));
+                }
+                _ => {
+                    return Err(RequestError::new(
+                        axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                        "Internal Server Error",
+                    ));
+                }
+            }
+        }
     }
-
-    Ok(response)
 }
